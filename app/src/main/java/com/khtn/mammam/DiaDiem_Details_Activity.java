9a8255c186e234288a473af84c96d1d6321111fe
Cường Lê <cuonglh2807@gmail.com>
2017-01-04 23:53:10 +0700

    Add new Activity - DiaDiem Details

---
+++ app/src/main/java/com/khtn/mammam/DiaDiem_Details_Activity.java
@@ -0,0 +1,14 @@
+package com.khtn.mammam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiaDiem_Details_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dia_diem_details);
+        
+    }
+}
