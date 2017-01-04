5af3d76a1e4acac9e1e05b98110cf59bccabefda
Cường Lê <cuonglh2807@gmail.com>
2017-01-04 23:41:10 +0700

    Cường Lê - Change minSDK from 9 to 21 - Done loading screen - MainActivity

--- app/src/main/java/com/khtn/mammam/MainActivity.java
+++ app/src/main/java/com/khtn/mammam/MainActivity.java
@@ -1,13 +1,37 @@
 package com.khtn.mammam;
 
+import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
+    private ProgressBar loading_wheel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        loading_wheel = (ProgressBar) findViewById(R.id.progressBar1);
+        loading_wheel.setVisibility(View.VISIBLE);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
