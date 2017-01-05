4d5e7f6ba676bd29517547dd692298f2fceaba32
Cường Lê <cuonglh2807@gmail.com>
2017-01-05 14:01:02 +0700

    Done Features - Commet - Direction - Share

--- app/src/main/java/com/khtn/mammam/DiaDiem_Details_Activity.java
+++ app/src/main/java/com/khtn/mammam/DiaDiem_Details_Activity.java
@@ -2,7 +2,9 @@ package com.khtn.mammam;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.TypedArray;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +13,11 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Gallery;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class DiaDiem_Details_Activity extends AppCompatActivity {
 
@@ -25,8 +31,11 @@ public class DiaDiem_Details_Activity extends AppCompatActivity {
             R.mipmap.a6,
             R.mipmap.a7};
 
-    @Override
+    private ImageView imgShare,imgDirection,imgComment;
 
+    private LinearLayout layoutHidden;
+    private TextView txtTextHidden;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dia_diem_details);
@@ -38,6 +47,64 @@ public class DiaDiem_Details_Activity extends AppCompatActivity {
             ImageView icon = (ImageView) singleFrame.findViewById(R.id.icon);
             icon.setImageResource(thumbnails[i]);
             scrollViewgroup.addView(singleFrame);
+        }
+
+        imgShare = (ImageView) findViewById(R.id.imgViewShare);
+        imgShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,"Check-in, tui đang ở nhà hàng Biển Đông 05 nè");
+                sendIntent.setType("text/plain");
+                startActivity(Intent.createChooser(sendIntent,"Chia sẻ cho bạn bè :"));
+            }
+        });
+
+        imgDirection = (ImageView) findViewById(R.id.imgViewDirection);
+        imgDirection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                String label = "Nhà hàng biển đông";
+//                String uriBegin = "geo:10.7590109,106.6772865";
+//                String query = "10.7590109,106.6772865(" + label + ")";
+//                String encodedQuery = Uri.encode(query);
+//                String uriString = uriBegin + "?q=" + encodedQuery;
+//                Uri uri = Uri.parse(uriString);
+//                Intent intent = new Intent(android.content.Intent.ACTION_VIEW, uri);
+//                startActivity(intent);
+
+                Uri gmmIntentUri = Uri.parse("google.navigation:q=Quán biển đông 5");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+
+        imgComment = (ImageView) findViewById(R.id.imgViewComment);
+        imgComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(DiaDiem_Details_Activity.this,CommentActivity.class);
+                startActivityForResult(i,99);
+            }
+        });
+
+        txtTextHidden = (TextView) findViewById(R.id.txtTextHidden);
+        layoutHidden = (LinearLayout) findViewById(R.id.layoutHidden);
+
+        layoutHidden.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==88)
+        {
+            //Toast.makeText(DiaDiem_Details_Activity.this,data.getStringExtra("noidungbinhluan"),Toast.LENGTH_LONG).show();
+            txtTextHidden.setText("NGỌC TRINH\n05/01/2017 07:15 PM\n"+data.getStringExtra("noidungbinhluan"));
+            layoutHidden.setVisibility(View.VISIBLE);
         }
     }
 }
